8271f3a876c0a3d8570b1c85d4620521d030f04a
Joy khulbe <khulbejoy.jk@gmail.com>
2024-10-23 19:39:48 +0530

    wrapped with main

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <ScrollProvider>
-      <Hero />
-    </ScrollProvider>
+    <main className="relative bg-black">
+      <ScrollProvider>
+        <Hero />
+      </ScrollProvider>
+    </main>
   );
 }
